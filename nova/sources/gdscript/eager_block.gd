4f365d36dd5f924eb002f3b8fdf9787c051c4f86
gyf1214 <gyf1214@gmail.com>
2024-03-17 10:43:02 -0400

    Add more eager block functions

--- nova/sources/gdscript/eager_block.gd
+++ nova/sources/gdscript/eager_block.gd
@@ -19,6 +19,7 @@ func _try_get_local_name(name):
     else:
         return name
 
+## define a node
 func label(name: String, display_name=null) -> void:
     if display_name == null:
         if _last_display_name == null:
@@ -28,18 +29,75 @@ func label(name: String, display_name=null) -> void:
     else:
         _last_display_name = display_name
 
+    name = _try_get_local_name(name)
+
     if _script_loader.IsDefaultLocale:
         _script_loader.RegisterNewNode(name, display_name)
     else:
         _script_loader.AddLocalizedNode(name, display_name)
 
+## jump to the given destination
+## should be called at the end of the node
+func jump_to(dest: String) -> void:
+    dest = _try_get_local_name(dest)
+    _script_loader.RegisterJump(dest)
+
+## add branches to the current node
+## should be called at the end of the node
+## should be called only once for each node, i.e., all branches of the node should be added at once
+## branches should be an array of 'branch'. A 'branch' is a dict with the following structure:
+## {
+##    dest: "name of the destination node"
+##    text: "text on the button", should not use if mode is jump
+##    image: {name: "image_name", x: x, y: y, scale: scale}, should not use if mode is jump
+##    mode: "normal|jump|show|enable", optional, default is normal
+##    cond: an expression string that returns a bool, should not use if mode is show
+## }
+func branch(branches: Array) -> void:
+    for i in branches.size():
+        var entry = branches[i]
+
+        var name = str(i)
+        var dest = _try_get_local_name(entry["dest"])
+        var image_info = entry.get("image", null)
+        var mode = entry.get("mode", "normal")
+        var cond = entry.get("cond", "")
+
+        if _script_loader.IsDefaultLocale:
+            _script_loader.RegisterBranch(name, dest, entry["text"], image_info, mode, cond)
+        else:
+            _script_loader.AddLocalizedBranch(name, dest, entry["text"])
+
+    _script_loader.EndRegisterBranch()
+
+## set the current node as a chapter
+func is_chapter() -> void:
+    _script_loader.SetCurrentAsChapter()
+
+## set the current node as a start node
+## a game can have multiple start points
+## which means this function can be called several times under different nodes
 func is_start() -> void:
     _script_loader.SetCurrentAsStart()
 
+## set the current node as a start point which is unlocked initially
+## indicates is_chapter() and is_start()
 func is_unlocked_start() -> void:
     _script_loader.SetCurrentAsChapter()
     _script_loader.SetCurrentAsUnlockedStart()
 
+## set the current node as a debug node
+## debug nodes can be entered by holding debug key in start game
+func is_debug() -> void:
+    _script_loader.SetCurrentAsDebug()
+
+## set the current node as an end node
+## should be called at the end of the node
+## a name can be assigned to an end point, which can differ from the node name
+## the name should be unique among all end point names
+## if no name is given, the name of the current node will be used
+## all nodes without child nodes should be marked as end nodes
+## if is_end() is not called under those nodes, they will be marked as end nodes automatically
 func is_end(name=null) -> void:
     name = _try_get_local_name(name)
     _script_loader.SetCurrentAsEnd(name)
