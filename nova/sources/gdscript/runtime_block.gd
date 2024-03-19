49fc898bff9f6a7629097b7b95777e7506765c9c
gyf1214 <gyf1214@gmail.com>
2024-03-19 19:11:00 -0400

    Implement move and tint

--- nova/sources/gdscript/runtime_block.gd
+++ nova/sources/gdscript/runtime_block.gd
@@ -19,16 +19,42 @@ func _get_obj(obj):
     return obj
 
 func _get_index(arr: Array, index: int, default=null):
-    return arr[index] if index < len(arr) else default
+    return arr[index] if index < len(arr) and arr[index] != null else default
 
-func move(obj, coord) -> void:
-    if !(coord is Vector3):
-        var z = _get_index(coord, 2, obj.position.z)
-        coord = Vector3(coord[0], coord[1], z)
-    obj.position = coord
+func _get_vec3(input, default: Vector3, single_default=null) -> Vector3:
+    if input is Vector3:
+        return input
+    elif (input is int or input is float) and single_default != null:
+        return single_default.call(input)
+    elif input != null:
+        var x = _get_index(input, 0, default.x)
+        var y = _get_index(input, 1, default.y)
+        var z = _get_index(input, 2, default.z)
+        return Vector3(x, y, z)
+    else:
+        return default
+
+func move(obj, coord, scale=null, angle=null) -> void:
+    obj = _get_obj(obj)
+    if coord != null and not coord is Vector3:
+        if scale == null:
+            scale = _get_vec3(_get_index(coord, 2, null), obj.scale,
+                func(s): return Vector3(s, s, 1))
+        if angle == null:
+            angle = _get_vec3(_get_index(coord, 4, null), obj.rotation_degrees,
+                func(s): return Vector3(0, 0, s))
+        coord = _get_vec3(coord, obj.position)
+
+    if coord != null:
+        obj.position = coord
+    if scale != null:
+        obj.scale = scale
+    if angle != null:
+        obj.rotation_degrees = angle
 
 func tint(obj, color) -> void:
-    if !(color is Color):
+    obj = _get_obj(obj)
+    if not color is Color:
         var a = _get_index(color, 3, 1)
         color = Color(color[0], color[1], color[2], a)
     obj.modulate = color
